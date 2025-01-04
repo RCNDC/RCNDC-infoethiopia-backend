a5581b1a57b70595125b0ce671b01419a3b8e8b6
Yohannes Teshome <kateyohannes@gmail.com>
2025-01-04 05:00:38 -0500

    add config file

---
+++ config/config.js
@@ -0,0 +1,24 @@
+{
+  "development": {
+    "username": "infoethiopianet_companyPortaluser",
+    "password": "2WrDgQcw2mF2",
+    "database": "infoethiopianet_companyPortal",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "infoethiopianet_companyPortaluser",
+    "password": "2WrDgQcw2mF2",
+    "database": "infoethiopianet_companyPortal",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+
